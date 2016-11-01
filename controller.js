f24e5840b926c984fac80627b3c7fa7621249ebc
mkrajews <mimikrajewski@Jakes-MacBook-Pro-2.local>
2016-10-31 19:44:06 -0600

    update mini userProfiles2

--- controller.js
+++ controller.js
@@ -1,10 +1,17 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
-
+  	mainService.getUsers().then(function(response) {
+      $scope.users = response.data.data;
+    });
+  };
 
   $scope.getUsers();
 
 });
+
+OR
+var promise = mainService.getUsers();
+promise.then(function(response){
+  scope.users = response.data.data;
+})
